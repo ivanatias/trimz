d639424bc2e732d61cd1d64e561fbae75fa691a0
Ivan Atias <ivan.d.atias@gmail.com>
2024-03-19 03:01:06 -0400

    🔧 refactor: modify navbar styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,15 @@
 export function Navbar() {
   return (
     <header class='flex justify-center'>
-      <nav class='fixed w-full flex items-center justify-between max-w-5xl px-5'>
+      <nav class='fixed w-full flex items-center justify-between max-w-4xl px-5'>
         <a
-          class='font-bold text-3xl italic text-black'
+          class='text-5xl italic bg-gradient-to-r from-orange-300 via-rose-400 to-emerald-300 bg-clip-text text-transparent'
           href='/'
           title='Go to homepage'
         >
           trimz.
         </a>
-        <a class='font-bold text-lg text-black' href='#'>
+        <a class='font-bold text-lg text-black' href='/sign-in'>
           Sign in
         </a>
       </nav>
